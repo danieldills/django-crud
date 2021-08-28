30ba4280b92471b4eeab1fa5782e17bf5bc52fe5
Daniel Dills <dills253@gmail.com>
2021-08-28 14:44:05 -0700

    updated settings and urls.py, successfully tested server

---
+++ snacks/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Snack
+
+class SnackListView(ListView):
+  pass
+
+class SnackDetailView(DetailView):
+  pass
+
+class SnackCreateView(CreateView):
+  pass
+
+class SnackUpdateView(UpdateView):
+  pass
+
+class SnackDeleteView(DeleteView):
+  pass
